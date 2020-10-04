b7474e3d14ccb0f3095d373b7d7a9a340d48f56c
Amir <amirzhan.tolebay@gmail.com>
2020-10-04 22:57:19 +0600

    Добавьте файлы проекта.

---
+++ BookStore/Models/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class StoreContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
